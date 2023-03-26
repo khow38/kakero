93b37856c773153211c19ee24e34d5a682280283
Chiyoung Lee <khow@haedosa.xyz>
2023-03-26 22:04:59 +0900

    [add] sepolia testnet

--- smartcontract/hardhat.config.js
+++ smartcontract/hardhat.config.js
@@ -13,9 +13,11 @@ require("dotenv").config()
 const MAINNET_RPC_URL =
     process.env.MAINNET_RPC_URL ||
     process.env.ALCHEMY_MAINNET_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+      "https://eth-mainnet.g.alchemy.com/v2/your-api-key"
+const sepolia_rpc_url =
+      process.env.goerli_rpc_url || "https://eth-sepolia.g.alchemy.com/v2/your-api-key"
+const goerli_rpc_url =
+      process.env.goerli_rpc_url || "https://eth-goerli.g.alchemy.com/v2/your-api-key"
 const POLYGON_MAINNET_RPC_URL =
     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
@@ -48,6 +50,15 @@ module.exports = {
             //   },
             saveDeployments: true,
             chainId: 5,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 11155111,
         },
         mainnet: {
             url: MAINNET_RPC_URL,
--- smartcontract/helper-hardhat-config.js
+++ smartcontract/helper-hardhat-config.js
@@ -13,10 +13,19 @@ const networkConfig = {
         lottoEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
     },
+    11155111: {
+        name: "sepolia",
+        subscriptionId: "719",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        keepersUpdateInterval: "30",
+        lottoEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625"
+    },
     5: {
         name: "goerli",
         subscriptionId: "9237",
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 150 gwei
         keepersUpdateInterval: "30",
         lottoEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
