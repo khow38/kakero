81f6503bb504bdb6aac26ee5773fb5ed57c226d8
ChiyoungLee <khow@haedosa.xyz>
2023-05-14 17:42:31 +0900

    [mod] players private -> public, dueDate modifier

--- smartcontract/contracts/BinaryBet.sol
+++ smartcontract/contracts/BinaryBet.sol
@@ -36,7 +36,7 @@ contract Betting {
         uint16 teamSelected;
     }
     // Address of the player and => the user info
-    mapping(address => Player) private playerInfo;
+    mapping(address => Player) public playerInfo;
     address payable[] private players;
     address payable[] private winners;
 
@@ -68,37 +68,45 @@ contract Betting {
         owner = payable(admin);
     }
 
-    function betOnOption1() public payable {
+    function betOnOption1() public payable onlyBeforeMatchDate {
         //The first require is used to check if the player already exist
-        require(!checkPlayerExists(msg.sender));
         require(!gameFinished);
         require(msg.value >= minimumBet, "Insufficient bet amount");
+        require(!checkPlayerExists(msg.sender));
+
+        //We charge the commission of the betting
+        uint256 betValue = (msg.value * (100 - commission)/100);
+
         //We set the player informations : amount of the bet and selected team
-        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].amountBet = betValue;
         playerInfo[msg.sender].teamSelected = 1;
 
         //then we add the address of the player to the players array
         players.push(payable(msg.sender));
 
         //at the end, we increment the stakes of the team selected with the player bet
-        totalBetOne += (msg.value * (100 - commission)/100);
+        totalBetOne += betValue;
         emit BettingEnter(msg.sender);
     }
 
-    function betOnOption2() public payable {
+    function betOnOption2() public payable onlyBeforeMatchDate {
         //The first require is used to check if the player already exist
-        require(!checkPlayerExists(msg.sender));
         require(!gameFinished);
         require(msg.value >= minimumBet, "Insufficient bet amount");
+        require(!checkPlayerExists(msg.sender));
+
+        //We charge the commission of the betting
+        uint256 betValue = (msg.value * (100 - commission)/100);
+
         //We set the player informations : amount of the bet and selected team
-        playerInfo[msg.sender].amountBet = msg.value;
+        playerInfo[msg.sender].amountBet = betValue;
         playerInfo[msg.sender].teamSelected = 2;
 
         //then we add the address of the player to the players array
         players.push(payable(msg.sender));
 
         //at the end, we increment the stakes of the team selected with the player bet
-        totalBetTwo += (msg.value * (100 - commission)/100);
+        totalBetOne += betValue;
         emit BettingEnter(msg.sender);
     }
 
@@ -138,6 +146,7 @@ contract Betting {
         owner.transfer(address(this).balance);
 
         gameFinished = true;
+
     }
 
     /**
@@ -184,6 +193,7 @@ contract Betting {
         distributePrizes(oraclemsg);
     }
 
+
     /** Getter Functions */
 
     function getAdmin() public view returns (address) {
@@ -196,15 +206,13 @@ contract Betting {
                        , string memory _option2Name
                        , string memory _option2LeagueName
                        , uint256 _matchDateTimestamp) public onlyAdmin{
+
         // Delete all the players
         for (uint256 i = 0; i < players.length; i++){
             delete playerInfo[players[i]];
         }
         delete players; // Delete all the players array
         delete winners; // Delete all the winners array
-        // players.length = 0; // Delete all the players array
-        LoserBet = 0; //reinitialize the bets
-        WinnerBet = 0;
         totalBetOne = 0;
         totalBetTwo = 0;
 
@@ -277,6 +285,11 @@ contract Betting {
 
     function getCommissionRatio() public view returns (uint256) {
         return commission;
+    }
+
+    modifier onlyBeforeMatchDate() {
+        require(block.timestamp < matchDateTimestamp, "The deadline has passed");
+        _;
     }
 
     modifier onlyAdmin {
